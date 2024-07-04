76d3220448c9cdcd3cbb030d57d51c0fc81959e9
abhi08-04 <abhinav.live08@gmail.com>
2024-07-04 17:34:14 +0530

    Create DSU_Set Implementation.cpp

---
+++ Graph Algorithms/Disjoint Set Union/DSU_Set Implementation.cpp
@@ -0,0 +1,33 @@
+    struct SetDSU {
+        int n;
+        vector<set<int>> customSet;
+        vector<int> elementIn;
+
+        SetDSU(int _n) {
+            n = _n;
+            customSet.resize(n);
+            elementIn.resize(n);
+
+            for (int i = 0; i < n; i++) {
+                elementIn[i] = i;
+                customSet[i].insert(i);
+            }
+        }
+
+        int find_set(int node) { return elementIn[node]; }
+
+        void unite(int u, int v) {
+
+            int setx = find_set(u);
+            int sety = find_set(v);
+
+            if(setx == sety) return;
+
+            if (customSet[setx].size() < customSet[sety].size()) swap(setx, sety);
+            for (auto ele : customSet[sety]) {
+                elementIn[ele] = setx;
+                customSet[setx].insert(ele);
+            }
+            customSet[sety].clear();
+        }
+    };
